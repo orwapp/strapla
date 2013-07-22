707d435856c4f56c0b5f837938a8f2e6fdf055b7
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-07-22 14:13:41 +0200

    added screenshot of GitLink to the request show page.

--- app/assets/javascripts/requests.js.coffee
+++ app/assets/javascripts/requests.js.coffee
@@ -1,3 +1,7 @@
 $ ->
   $("#expert_carousel").each ->
     $(this).carousel interval: false
+
+
+  $(document).on 'click', '.browse_code', (e) => 
+    $('.code_details').slideToggle()
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,8 @@ class Request < ActiveRecord::Base
   belongs_to :request_group
   belongs_to :user
   validates_presence_of :subject, :goal, :request_group
+  attr_accessor :repository_url
 
   scope :unassigned, -> { where user_id: nil }
+
 end
