af83045ec182a1edd8e71b1524282c3a724fd4f1
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-07-24 16:38:04 +0200

    basic user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid from Fabrication" do
+    @user = Fabricate(:user)
+    @user.should be_valid
+  end
 end
