32868c72b2d4b9cb75ef20ce79b39e5ed68657c9
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-07-24 12:04:15 +0200

    a Request has many PriceQuotes

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,7 @@
 class Request < ActiveRecord::Base
   belongs_to :request_group
   belongs_to :user
+  has_many :price_quotes
   validates_presence_of :subject, :goal, :request_group, :user
   attr_accessor :repository_url
 
