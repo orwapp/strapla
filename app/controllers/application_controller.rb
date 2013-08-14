b31454699ca312cd83e8828dce764fd15fd0d247
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-08-14 07:36:27 +0200

    @current_user is always available.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_current_user
 
   http_basic_authenticate_with name: ENV['HTTP_USERNAME'] || 'funky_admin' , password: ENV['HTTP_PASSWORD'] || 'top_secret'
   
-  
+
 
   protected
 
+  def set_current_user
+    @current_user ||= current_user if user_signed_in?
+  end
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :phone, :bio, :image_url, :name) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :phone, :bio, :image_url, :name) }
