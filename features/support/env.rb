4d5319d04e6bdebd5030e60d565374288d271845
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-08-22 14:18:42 +0200

    Cucumber runs

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,9 @@
 #require 'cucumber/rails'
 #require 'rubygems'
-#require 'bundler'
-#
+require 'capybara'
+require 'bundler'
+require 'database_cleaner'
+
 Bundler.setup :default, :development, :test
 #require "#{Rails.root}/spec/fabricators/*"  
 #require "#{Rails.root}/spec/fabricators/**/*fabricator.rb
@@ -31,13 +33,4 @@ Capybara.default_selector = :css
 #
 #ActionController::Base.allow_rescue = false
 
-# Remove/comment out the lines below if your app doesn't have a database.
-# For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
-begin
-  DatabaseCleaner.strategy = :transaction
-rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-end
-
-#Cucumber::Rails::Database.javascript_strategy = :truncation
 
