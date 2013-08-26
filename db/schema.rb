e8ad0fc34f2ab53195087bd8d5b81e7a67310689
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-08-26 20:23:33 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130824164259) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "background_informations", force: true do |t|
     t.text     "elevator_pitch"
     t.text     "background_information"
