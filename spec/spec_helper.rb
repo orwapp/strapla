b3d06bb64b766a0a3b2cc525ac98cd807c254e6a
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-08-29 11:26:24 +0200

    update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
 #require 'email_spec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -17,18 +19,35 @@ RSpec.configure do |config|
   config.include(EmailSpec::Helpers)
   config.include(EmailSpec::Matchers)
   config.include Warden::Test::Helpers
-  
+  config.include Capybara::DSL
+
+  config.include(MailerMacros)
+  config.before(:each) { reset_email }
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
 
 
-config.after(:all) do
-  Warden.test_reset!
-end
-
-include Warden::Test::Helpers
-Warden.test_mode!
+#config.after(:all) do
+#  Warden.test_reset!
+#end
+#
+#include Warden::Test::Helpers
+#Warden.test_mode!
