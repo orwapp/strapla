f149d600fb70b10abff8544fa9752ab84802b4bd
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-08 18:58:26 +0200

    deprecated module

--- spec/fabricators/background_information_fabricator.rb
+++
@@ -1,9 +0,0 @@
-Fabricator(:background_information) do
-  elevator_pitch           "MyText"
-  background_information   "MyText"
-  no_list                  "MyText"
-  what_can_go_wrong        "MyText"
-  what_is_it_going_to_give "MyText"
-  what_is_the_frame        "MyText"
-  request                  nil
-end
