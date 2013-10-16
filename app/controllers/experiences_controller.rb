4487878b33d25c5cd45863f5420ebeee95cd5795
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-16 10:26:12 +0200

    Adding the experience form

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,25 @@
 class ExperiencesController < ApplicationController
+  def create
+    @experience = @current_user.experiences.new(page_params)
+
+    respond_to do |format|
+      if @experience.save
+        format.html { redirect_to :back }
+        format.json { render action: 'show', status: :created, location: @experience }
+      else
+        format.html { redirect_to :back }
+        format.json { render json: @experience.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+	def show
+	end
+
+	private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def page_params
+    params.require(:experience).permit(:title, :company, :from_date, :to_date, :summary)
+  end
+
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,4 @@
 class Experience < ActiveRecord::Base
   belongs_to :user
+	validates_presence_of :title, :company, :summary
 end
