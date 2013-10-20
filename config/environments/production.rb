1aad9e76fcef42eab8bc037df4f34fea4a2e3af9
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-20 21:06:36 +0200

    Use ENV['domain'] for default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ CodeRunner::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'www.coderunner.co' }
+  config.action_mailer.default_url_options = { :host => ENV['domain']  }
 end
