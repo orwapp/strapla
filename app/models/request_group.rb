362c926abd4ff5f09665579da80e8b1084404e7b
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-24 20:14:01 +0200

    listing images and radio buttons.

--- app/models/request_group.rb
+++ app/models/request_group.rb
@@ -1,14 +1,15 @@
 class RequestGroup < ActiveRecord::Base
-  require 'string'
+  require 'String'
 
   has_many :requests
   validates_presence_of :title
-  #validates_uniqueness_of :title
   has_and_belongs_to_many :users
 
   def image_url
-    self.title.gsub!(/[^0-9a-z ]/i, '')
+    self.title.gsub(/[^0-9a-z ]/i, '')
+    self.title.gsub(/ /, '_')
     self.title.downcase.normalize + ".png"
   end
+
 
 end
--- lib/String.rb
+++ lib/String.rb
@@ -3,6 +3,7 @@ class String
 	def normalize
     self.gsub(/[^0-9a-z ]/i, '')
     self.gsub(/\s/, '')
+    self.gsub(/\//, '')
 	end
 
 end
