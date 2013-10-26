aabaff95718e6af1cba1f1374c0997d6087058ed
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-26 05:20:58 +0200

    uppercase ENV var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ CodeRunner::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => ENV['domain']  }
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN']  }
 end
