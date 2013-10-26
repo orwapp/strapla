093117ece9832c3e8633cf485721f672a907b7e1
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-26 04:26:19 +0200

    removed Ruby as a RequestGroup

---
+++ db/migrate/20131026021429_remove_ruby_from_the_request_groups.rb
@@ -0,0 +1,12 @@
+class RemoveRubyFromTheRequestGroups < ActiveRecord::Migration
+  def change
+    ruby_group = RequestGroup.where(title: 'Ruby').first
+    web_group  = RequestGroup.where(title: 'Web application').first
+
+    Request.where(request_group_id: ruby_group.id).all do |request|
+      request.update_attribute(request_group_id: web_group.id)
+    end
+
+    RequestGroup.where(title: 'Ruby').first.destroy
+  end
+end
