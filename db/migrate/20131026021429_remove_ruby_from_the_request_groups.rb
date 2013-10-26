b4add7a363a95f796bf62a11b420b97a7d26fada
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-10-26 04:47:36 +0200

    Dont freak out if the Ruby group is missing

--- db/migrate/20131026021429_remove_ruby_from_the_request_groups.rb
+++ db/migrate/20131026021429_remove_ruby_from_the_request_groups.rb
@@ -2,6 +2,7 @@ class RemoveRubyFromTheRequestGroups < ActiveRecord::Migration
   def change
     ruby_group = RequestGroup.where(title: 'Ruby').first
     web_group  = RequestGroup.where(title: 'Web application').first
+    return unless ruby_group
 
     Request.where(request_group_id: ruby_group.id).all do |request|
       request.update_attribute(request_group_id: web_group.id)
