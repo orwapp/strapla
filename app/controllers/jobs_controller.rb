862ccf6a04b1ddd8ae372edd4f150b3c2d9a7160
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-11-10 16:01:36 +0100

    add current to job_params permit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -74,6 +74,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-			params.require(:job).permit(:title, :company, :summary, :from_date, :to_date)
+			params.require(:job).permit(:title, :company, :summary, :from_date, :to_date, :current)
     end
 end
