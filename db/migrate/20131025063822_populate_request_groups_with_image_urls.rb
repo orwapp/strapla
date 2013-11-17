cc0e8bf9bc6c6d1cd2f6abc6e656b0d24af4b83c
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-11-17 15:33:12 +0100

    Renamed the desktop app groups.

--- db/migrate/20131025063822_populate_request_groups_with_image_urls.rb
+++ db/migrate/20131025063822_populate_request_groups_with_image_urls.rb
@@ -1,7 +1,7 @@
 class PopulateRequestGroupsWithImageUrls < ActiveRecord::Migration
   def up
-    RequestGroup.where(title: 'Desktop application - Windows').first_or_create.update_attribute(:image_url, 'language_and_frameworks/windows.png')
-    RequestGroup.where(title: 'Desktop application - Mac').first_or_create.update_attribute(:image_url, 'language_and_frameworks/mac.png')
+    RequestGroup.where(title: 'Windows application').first_or_create.update_attribute(:image_url, 'language_and_frameworks/windows.png')
+    RequestGroup.where(title: 'Mac application').first_or_create.update_attribute(:image_url, 'language_and_frameworks/mac.png')
     RequestGroup.where(title: 'Android app').first_or_create.update_attribute(:image_url, 'language_and_frameworks/android.png')
     RequestGroup.where(title: 'iOS app').first_or_create.update_attribute(:image_url, 'language_and_frameworks/ios.png')
     RequestGroup.where(title: 'Integration').first_or_create.update_attribute(:image_url, 'language_and_frameworks/integration.png')
