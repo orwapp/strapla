593d20aab800f644c349d62f38c6e3404faef425
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-12-13 15:07:51 +0100

    Image upload to S3 works

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     #:host                   => 's3.example.com',
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'images'                     # required
+  config.fog_directory  = 'stabstr'                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
