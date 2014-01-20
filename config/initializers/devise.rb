4f0efaee73bbb2a4b80b17141493c6e9d10a1693
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-01-20 16:59:51 +0100

    signout via DELETE

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -215,7 +215,6 @@ Devise.setup do |config|
   # config.navigational_formats = ["*/*", :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :get
   config.sign_out_via = Rails.env.test? ? :get : :delete
 
 
