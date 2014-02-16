d49af5ab41a3506151be933b9a3b0a97a5afd5ea
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-02-16 12:20:13 +0100

    image can be deleted

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -58,7 +58,7 @@ class ImagesController < ApplicationController
   def destroy
     @image.destroy
     respond_to do |format|
-      format.html { redirect_to images_url }
+      format.html { redirect_to :back }
       format.json { head :no_content }
     end
   end
