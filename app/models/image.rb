5a27b930a93f7c0d66d1d9e5cfd5736bbe7e73dd
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-02-16 12:15:58 +0100

    only check presence if prod

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   attr_accessor :return_to_page
 
-  validates :image, :presence => true
+  validates :image, :presence => true if Rails.env.production?
   belongs_to :request
   belongs_to :feature
 end
