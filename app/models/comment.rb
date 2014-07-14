0013508084c1e948883def752690fe441077bdde
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-07-14 10:36:43 +0200

    put comment test as pending.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :price_quote
   belongs_to :user
-  #validates_presence_of :price_quote
+  validates_presence_of :price_quote
   validates_presence_of :user
 
   after_save :send_email
 
   private
   def send_email
+    puts "send_email"
     return unless price_quote.present?
     if price_quote.request.user != user
       to   = price_quote.request.user
@@ -16,6 +17,7 @@ class Comment < ActiveRecord::Base
       to   = price_quote.user
       from = price_quote.request.user
     end
+    puts "new_comment "
     UserMailer.new_comment(from,to, self).deliver
   end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,30 +1,33 @@
 require 'spec_helper'
 
-describe Comment do
-  #before do
-  #  @request_owner = Fabricate(:user)
-  #  @expert        = Fabricate(:user)
-  #  @price_quote   = Fabricate(:price_quote, user: @expert)
-  #  @comment = Fabricate(:comment, price_quote: @price_quote)
-  #end
-  #specify { @comment.should be_valid }
+describe Comment, focus: true do
+  before do
+    @request_owner = Fabricate(:user)
+    @expert        = Fabricate(:user)
+    @price_quote   = Fabricate(:price_quote, user: @expert)
+    @comment = Fabricate(:comment, price_quote: @price_quote)
+  end
+  specify { @comment.should be_valid }
 
-  #it "belongs to a Price Quote" do
-  #  @comment.price_quote.should      be_kind_of PriceQuote
-  #  @comment.price_quote.user.should be_kind_of User
-  #end
+  it "belongs to a Price Quote" do
+    @comment.price_quote.should      be_kind_of PriceQuote
+    @comment.price_quote.user.should be_kind_of User
+  end
 
-  describe "When creating new comments" do
-    let(:request_owner) { Fabricate(:user) }
-    let(:expert)        { Fabricate(:user) }
+  pending "When creating new comments" do
+    let(:request_owner) { Fabricate(:user, email: 'request@owner.com') }
+    let(:expert)        { Fabricate(:user, email: 'expert@user.com') }
     let(:request)       { Fabricate(:request, user: expert) }
-    let(:price_quote)   { Fabricate(:price_quote, user: expert, request: request) }
-    let(:comment)       { Fabricate(:comment, price_quote: price_quote) }
+    let(:price_quote)   { Fabricate(:price_quote, user: expert, 
+                                    request: request) }
+    let(:comment)       { Fabricate(:comment, price_quote: price_quote, 
+                                   user: expert) }
 
-    it "Sends an email to the request owner when the expert comments on a price quote" do
-      pending
+
+    it "Sends an email to the request owner when the expert comments on a 
+        price quote" do
       ActionMailer::Base.deliveries.last.to.should include(request_owner.email)
-      #mail.subject.should eq("New comment")
+      mail.subject.should eq("New comment")
       #mail.to.should eq([request_owner.email])
       #mail.from.should eq(["system@coderunner.co"])
       #mail.body.encoded.should match((request_url(request)))
