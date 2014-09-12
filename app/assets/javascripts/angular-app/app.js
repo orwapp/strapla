757009800f4d9e6aea377aaf7973d46ed37cb802
Martin Stabenfeldt <martin@stabenfeldt.net>
2014-09-12 08:56:37 +0200

    > 80 chars and whitespace

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,6 +1,7 @@
 angular
   .module('app', ['ngRoute', 'restangular'])
-  .config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
+  .config(['$routeProvider', 'RestangularProvider', 
+  function($routeProvider, RestangularProvider) {
     // CRUD API not implemented yet. Will use Grape when needed.
     //RestangularProvider.setBaseUrl('/api/v1');
     // $routeProvider.when('/', {
